674d63c1a6843329fcd1b39a6f5bfcd34617557a
Trisa Dominic <dominictrisa@gmail.com>
2025-09-24 19:51:17 +0530

    Switch to bcryptjs for better compatibility

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // Change to bcryptjs
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -12,5 +13,23 @@ const userSchema = new mongoose.Schema({
   },
   createdAt: { type: Date, default: Date.now }
 });
+
+// Update password hashing
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update password comparison
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
