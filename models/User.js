5e5afe641760df7b0f870da821608e35cb58b42e
Trisa Dominic <dominictrisa@gmail.com>
2025-09-25 22:17:00 +0530

    Replace bcrypt with bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); // Change to bcryptjs
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: String,
